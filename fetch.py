f1fdb28e94ed15c77297d41b6eebc7614e89fc43
Rubén Laguna <ruben.laguna@gmail.com>
2025-03-11 16:02:31 +0100

    Merge applications and responses

--- fetch.py
+++ fetch.py
@@ -17,23 +17,43 @@ DATE_FROM = os.environ["DATE_FROM"]
 DATE_TO = os.environ["DATE_TO"]
 
 
+def fetch(endpoint):
+    params = {
+        "token": TOKEN,
+        "dateFrom": DATE_FROM,
+        "dateTo": DATE_TO,
+    }
+    url = f"{BASE_URL}/{endpoint}"
+    r = requests.get(url, params)
+    logger.info("Fetched %s", url)
+    result = r.json()
+    logger.info("Number of results: %s", len(result))
+    return result
+
+
 def main():
-    endpoints = ["applications", "responses"]
     params = {
         "token": TOKEN,
         "dateFrom": DATE_FROM,
         "dateTo": DATE_TO,
     }
 
+    applications = fetch("applications")
+    responses = fetch("responses")
+
+    responses_by_application_id = {}
+    for r in responses:
+        app_id = r.get('Application Id')
+        responses_by_application_id.setdefault(app_id, []).append(r)
+
+    print(responses_by_application_id.keys())
+    for a in applications:
+        app_id = a.get('Application Id')
+        a['responses'] = responses_by_application_id.get(appid)
 
 
-    for ep in endpoints:
-        url = f"{BASE_URL}/{ep}"
-        r = requests.get(f"{BASE_URL}/applications", params)
-        logger.info("Fetched %s", url)
-        result = r.json()
-        logger.info("Number of results: %s", len(result))
-        # print(r.json())
+    with  open('result.json', 'w') as f: 
+       pass 
 
 
 if __name__ == "__main__":
